bdbab7f46d2f8cd6a1ad0a2660b54faa08f047b8
godsboy404 <zrz515@outlook.com>
2024-12-20 11:34:47 +0800

    truly the last modify(

--- results/results.py
+++ results/results.py
@@ -4,13 +4,13 @@ import numpy as np
 def main():
     plt.rc("font", family='Microsoft YaHei')
     rewards_dqn_nature = np.loadtxt('rewards_per_episode_dqn_nature.csv', delimiter=',')
-    rewards_dqn_lonely = np.loadtxt('rewards_per_episode.csv', delimiter=',')
-    plt.plot(rewards_dqn_nature,label='Nature版')
-    plt.plot(rewards_dqn_lonely,label='自训练版')
+    rewards_dqn_neurips = np.loadtxt('rewards_per_episode_dqn_neurips.csv', delimiter=',')
+    #plt.plot(rewards_dqn_nature,label='Nature版')
+    plt.plot(rewards_dqn_neurips)
 
     plt.xlabel('Episode')
     plt.ylabel('Reward')
-    plt.title('Atari-Pong 每轮奖励（Nature版 Vs. 自训练版）')
+    plt.title('Atari-Pong 每轮奖励')
     plt.legend()
     plt.savefig('results_per_episode.png')
     plt.show()
