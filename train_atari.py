f180e95a757e44b890cf8ff4ce192616d2733a89
godsboy404 <zrz515@outlook.com>
2025-03-07 16:47:05 +0800

    ssh tst

--- train_atari.py
+++ train_atari.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
     hyper_params = {
         "seed": 42,  # which seed to use
-        "env": "PongNoFrameskip-v4",  # name of game
+        "env": "PongNoFrameskip-v4",  # name of the game
         "replay-buffer-size": int(5e3),  # replay buffer size
         "learning-rate": 1e-4,  # learning rate for Adam optimizer
         "discount-factor": 0.99,  # discount factor
